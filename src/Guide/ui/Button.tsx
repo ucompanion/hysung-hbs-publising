cf7178c94ecb6927134f403670dae35dffd13b8e
조찬기 <36979670+isuda7@users.noreply.github.com>
2025-04-22 16:16:37 +0900

    대소문자 파일/폴더 정리 초기화

--- src/Guide/ui/Button.tsx
+++
@@ -1,8 +0,0 @@
-
-import React from 'react';
-
-const Button = () => {
-  return <button style={{ padding: '10px', backgroundColor: 'blue', color: 'white' }}>Click me</button>;
-};
-
-export default Button;
--- src/Guide/ui/Icon.tsx
+++
@@ -1,8 +0,0 @@
-
-import React from 'react';
-
-const Icon = () => {
-  return <div>아이콘 마크업</div>;
-};
-
-export default Icon;
