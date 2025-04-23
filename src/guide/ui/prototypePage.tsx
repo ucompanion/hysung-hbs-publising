64cdba307070032b67e54108839c0d1552fedd60
ucomp-kimshinhye <kimshinhye@ucomp.co.kr>
2025-04-23 11:17:09 +0900

    가이드를 위한 프로토타입 페이지 작업중

--- src/guide/ui/prototypePage.tsx
+++ src/guide/ui/prototypePage.tsx
@@ -1,31 +1,96 @@
 'use client';
 
 import React from 'react';
-import Button from './button';
-import Accordion from './accordion';
-import Icon from './icon';
+// import Button from './button';
+// import Accordion from './accordion';
+// import Icon from './icon';
+
+import './css/guide.css';
 
 const PrototypePage = () => {
   return (
-    <div style={{ padding: "2rem", background: "pink" }}>
-      <h1>UI 가이드 프로토타입 페이지</h1>
+  // Container
+  <main className="g-container">
+      <div id="content" className="content">
+        <div className="content-body outer">
+          <div className="inner">
+            <h1 className="g-h1">Prototype</h1>
+          </div>
+          
+          {/* Section */}
+          <section className="g-section inner">
+            <div className="g-section-header">
+              <h2 className="g-h2">System</h2>
+            </div>
+            <div className="g-section-body">
+							<h3 className="g-h3">규칙</h3>
+              <div className="g-tbl-type1">
+                <table>
+                  <colgroup>
+                    <col style={{ width: '20%' }} />
+                    <col />
+                  </colgroup>
+                  
+                  <tbody>
+                  <tr>
+                        <th className="align-l">기본</th>
+                        <td className="align-l"><code className="g-code-label">data-bul-*</code></td>
+                      </tr>
+                      <tr>
+                        <th className="align-l">모듈</th>
+                        <td className="align-l"><code className="g-code-label">module-*</code></td>
+                      </tr>
+                  </tbody>
+                </table>
+              </div>
+              <h3 className="g-h3">기본</h3>
+              <pre>
+                <code>
+                  {`
+              <div></div>
+                  `}
+                </code>
+              </pre>
+            </div>
+          </section>
+          {/* Section */}
 
-      <section>
-        <h2>Button</h2>
-        <Button />
-      </section>
+          {/* Section */}
+          <section className="g-section inner">
+						<div className="g-section-header">
+							<h2 className="g-h2">Modules</h2>
+						</div>
+						<div className="g-section-body">
+							<h3 className="g-h3">모듈명</h3>
+							<h4 className="g-h4">module-a</h4>
+							<h5 className="g-h5">상세구분</h5>
+							<ul className="g-desc">
+								<li>목록</li>
+							</ul>
+						</div>
+					</section>
+          {/* Section */}
+        </div>
 
-      <section>
-        <h2>Accordion</h2>
-        <Accordion />
-      </section>
 
-      <section>
-        <h2>Icon</h2>
-        <Icon />
-      </section>
+        {/* <section>
+          <h2>Button</h2>
+          <Button />
+        </section>
 
-    </div>
+        <section>
+          <h2>Accordion</h2>
+          <Accordion />
+        </section>
+
+        <section>
+          <h2>Icon</h2>
+          <Icon />
+        </section> */}
+      </div>
+
+    </main>
+    // Container
   );
 };
 
